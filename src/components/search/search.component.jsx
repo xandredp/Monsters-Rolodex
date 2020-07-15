676891c586a56a87b11330e3236500e2c73b197e
Xandre Du Plooy <xandre@uproar.gg>
2020-07-16 09:54:59 +1200

    adding all files

---
+++ src/components/search/search.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './search.styles.css';
+
+export const Search = ({placeholder, handleChange}) => {
+	return (
+		<input
+			className="search"
+			type="search"
+			placeholder={placeholder}
+			onChange={handleChange}>
+		</input>
+	)
+}
